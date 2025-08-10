8644d53008299bc013d28ff9997a70281a1ab95e
chally-lang <54439978+chally-lang@users.noreply.github.com>
2025-08-10 18:28:14 +0100

    Update route.ts with new email reply functionality

--- src/app/api/admin/reply-message/route.ts
+++ src/app/api/admin/reply-message/route.ts
@@ -9,7 +9,8 @@ import { NextRequest } from "next/server";
 
 export async function POST(req: NextRequest) {
   try {
-    const { userId } = auth();
+    const session = await auth();
+    const { userId } = session;
     
     if (!userId) {
       return new Response(JSON.stringify({ error: "Unauthorized" }), { 
